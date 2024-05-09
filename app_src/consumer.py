7ce813b16573280166083f93bc6b03a26d20c6c5
ShinVu <datminh.vunguyen@gmail.com>
2024-05-09 14:35:30 +0700

    add consumer, producer, admin_client for kafka

---
+++ app_src/consumer.py
@@ -0,0 +1,38 @@
+
+from confluent_kafka import Consumer
+import json, time
+
+    
+def json_deserializer(data):
+    """
+    Deserialize json data
+
+    Args:
+        data: The data to deserialize.
+    """
+    return json.load(data)
+
+
+consumer_conf = {'bootstrap.servers': 'kafka:9092',
+            
+                     'auto.offset.reset': "earliest"}
+ 
+consumer = Consumer(consumer_conf)
+
+consumer.subscribe(['exercise_topic'])
+
+while True:
+    try:
+        # SIGINT can't be handled when polling, limit timeout to 1 second.
+        msg = consumer.poll(1.0)
+        if msg is None:
+            continue
+
+        user = json_deserializer(msg.value())
+
+        if user is not None:
+            print(user)
+    except KeyboardInterrupt:
+        break
+
+consumer.close()
--- app_src/producer.py
+++ app_src/producer.py
@@ -1,27 +1,65 @@
 from time import time
-from kafka import KafkaProducer
+from confluent_kafka import Producer
 from faker import Faker
 import json, time
 
 faker = Faker()
 
+def delivery_report(err, msg):
+    """
+    Reports the success or failure of a message delivery.
+
+    Args:
+        err (KafkaError): The error that occurred on None on success.
+        msg (Message): The message that was produced or failed.
+    """
+
+    if err is not None:
+        print("Delivery failed for User record {}: {}".format(msg.key(), err))
+        return
+    print('User record {} successfully produced to {} [{}] at offset {}'.format(
+        msg.key(), msg.topic(), msg.partition(), msg.offset()))
+    
+    
 def get_register():
+    """
+    Generate data for testing
+    """
     return {
         'name': faker.name(),
         'year' : faker.year()
     }
     
 def json_serializer(data):
+    """
+    Serialize json data
+
+    Args:
+        data: The data to serialize.
+    """
     return json.dumps(data).encode('utf-8')
 
-producer = KafkaProducer(
-    bootstrap_servers = ['172.22.0.2:9092'], # server name
-    value_serializer = json_serializer # function callable
-    )
+producer = Producer({
+    "bootstrap.servers" : "kafka:9092" # server name
+  }  )
 
 while True:
+    # Serve on_delivery callbacks from previous calls to produce()
+    producer.poll(0.0)
+    
+    # Generate data
     user = get_register()
-    producer.send(
-        'second_topic',user
-    )
+    
+    try:
+        producer.produce(
+            topic="exercise_topic",
+            value=json_serializer(user),
+            on_delivery = delivery_report
+        )
+    except KeyboardInterrupt:
+        break 
+    except ValueError:
+        print("Invalid input, discarding record...")
+        continue
+    
     time.sleep(3)
